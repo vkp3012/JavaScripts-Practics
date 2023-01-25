330732c0d47b6cf03d76f166f0ab9f396b8da06e
vkp3012 <vivekpatel301295@gmail.com>
2023-01-25 12:45:08 +0530

    join maethod is done

---
+++ practics/function/split.js
@@ -0,0 +1,22 @@
+// The split method splits a string into an array of strings.
+//  It takes an argument for the delimiter, which can be a character to use to break up the string or a regular expression.
+//   For example, if the delimiter is a space, you get an array of words, and if the delimiter is an empty string, 
+//   you get an array of each character in the string.
+
+// Here are two examples that split one string by spaces, then another by digits using a regular expression:
+
+const str = "Hello World";
+const bySpace = str.split(" ");
+
+const otherString = "How9are7you2today";
+const byDigits = otherString.split(/\d/);
+
+
+// /\W/ Matches any non-word character. This includes spaces and punctuation, but not underscores. 
+// It’s equivalent to /[^A-Za-z0-9_]/.
+
+function splitify(str) {
+    return str.split(/\W/)
+}
+
+splitify("Hello World,I-am code");
